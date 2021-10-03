fd40b5bb590ee77a9fa6c3462dd7b4db429a620e
Lucas Casotti <lucas.rookis@gmail.com>
2021-10-03 19:07:20 -0300

    Coloca a senha já como parâmetro da instalação

--- install.sh
+++ install.sh
@@ -21,3 +21,4 @@ sudo rm -rf /var/www/html/*
 sudo git clone https://github.com/lucascasotti/accel-ppp-webif.git /var/www/html
 sudo chown -R www-data:www-data /var/www/html/
 sudo su www-data -s /bin/bash -c "COMPOSER=/var/www/html/composer.json composer --working-dir=/var/www/html/ install"
+sudo su www-data -s /bin/bash -c "/usr/bin/php /var/www/html/data.php --password $1"
