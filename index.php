5a2d182d1b2e9e58e233d795360da2a916c37111
Lucas Casotti <lucas.rookis@gmail.com>
2021-10-03 20:03:43 -0300

    Importação dos envs no arquivo index

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php require_once(__DIR__.'/vendor/autoload.php'); ?>
+<?php require_once(__DIR__.'/vendor/autoload.php'); 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+?>
 <!doctype html>
 <html>
 
